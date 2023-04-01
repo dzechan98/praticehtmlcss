3f1b92510fe1af69dcca3952d70e456e9b3b7c7c
dzechan98 <dedoanpro@gmail.com>
2023-04-01 20:30:21 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,53 @@
+const animationElement = document.querySelectorAll(".show-on-scroll");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slideWrapper = document.querySelector(".slide-wrapper");
+const slideMain = document.querySelector(".slide_main");
+const slideItem = document.querySelectorAll(".slide__item");
+const slideWidth = slideWrapper.offsetWidth;
+let positionX = 0;
+let index = 0;
+function tonggleAnimationElementWindow(element) {
+  let rect = element.getClientRects()[0];
+  let heightScreen = window.innerHeight;
+  if (!(rect.bottom < 0 || rect.top > heightScreen)) {
+    element.classList.add("start");
+  } else {
+    element.classList.remove("start");
+  }
+}
+
+function checkAnimation() {
+  animationElement.forEach((el) => {
+    tonggleAnimationElementWindow(el);
+  });
+}
+
+function handleShow() {
+  slideMain.style.transform = `translateX(${positionX}px)`;
+}
+
+checkAnimation();
+
+window.onscroll = checkAnimation;
+
+nextBtn.onclick = function () {
+  index++;
+  console.log(slideWidth);
+  if (index > slideItem.length - 1) {
+    index = 0;
+    positionX = slideWidth;
+  }
+  positionX = positionX - slideWidth;
+  handleShow();
+};
+
+prevBtn.onclick = function () {
+  index--;
+  if (index < 0) {
+    index = slideItem.length - 1;
+    positionX = -(index + 1) * slideWidth;
+  }
+  positionX = positionX + slideWidth;
+  handleShow();
+};
